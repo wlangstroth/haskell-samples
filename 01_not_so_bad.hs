50f612f6998add4bd76d085c140295459d53df93
Will Langstroth <will@langstroth.com>
2011-10-18 19:17:36 -0400

    add note about do notation

--- 01_not_so_bad.hs
+++ 01_not_so_bad.hs
@@ -1,3 +1,7 @@
 main :: IO ()
 main = do
   putStrLn "There. That wasn't so bad, was it?"
+
+-- Note: the 'do' above doesn't need to go there. We could have written
+-- main = putStrLn "The same thing"
+-- but do notation is usually what happens in main, so we might as well.
