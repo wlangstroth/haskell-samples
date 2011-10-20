b083e392e06aaaa9f6a4aa420aaab18d82426462
Will Langstroth <will@langstroth.com>
2011-10-20 18:24:16 -0400

    add some comments to easy cat

--- 02_easy_cat.hs
+++ 02_easy_cat.hs
@@ -6,3 +6,7 @@ main = do
     [f] <- getArgs
     c <- readFile f
     putStrLn c
+
+-- Still doing okay. getArgs returns a list of things, and in this case,
+-- we only want the one thing we know is going to be there. It's not exactly
+-- the most robust program ever, but it's very readable.
