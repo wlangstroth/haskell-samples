b07fcae4a3902c09436b7efee598a7611b9910f4
Will Langstroth <will@langstroth.com>
2011-12-23 22:36:17 -0500

    add rpn example

---
+++ 06_rpn.hs
@@ -0,0 +1,15 @@
+import Data.List
+
+solveRPN :: String -> Float
+solveRPN = head . foldl foldingFunction [] . words
+  where
+    foldingFunction (x:y:ys) "*" = (x * y):ys
+    foldingFunction (x:y:ys) "+" = (x + y):ys
+    foldingFunction (x:y:ys) "-" = (y - x):ys
+    foldingFunction (x:y:ys) "/" = (y / x):ys
+    foldingFunction (x:y:ys) "^" = (y ** x):ys
+    foldingFunction (x:xs) "ln" = log x:xs
+    foldingFunction xs "sum" = [sum xs]
+    foldingFunction xs numberString = read numberString:xs
+
+-- Who doesn't love a good reverse Polish notation example?
