438a85ae81248cc0fdad8172f4357a99600d62fc
Will Langstroth <will@langstroth.com>
2012-10-05 09:28:58 -0400

    add hailstone

--- grab_bag.hs
+++ grab_bag.hs
@@ -1,8 +1,12 @@
--- These examples are personal favourites, using Haskell to do bizarre things
--- in short order.
+-- These examples are personal favourites
 
 -- is "radar" a palindrome?
 ((==) <*> reverse) "radar"
 
 -- a short fib definition
 fix ((0:) . scanl (+) 1)
+
+-- the hailstone algorithm
+hailstone x
+   | even x    = x `div` 2
+   | otherwise = 3 * x + 1
