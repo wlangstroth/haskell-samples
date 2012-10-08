3fb566a55485df51e0bc557daddfb9986f97ad32
Will Langstroth <will@langstroth.com>
2012-10-08 15:55:32 -0400

    add sorting

---
+++ 08_sorting.hs
@@ -0,0 +1,38 @@
+-- for those of you who enjoyed your algorithm courses, some sorting 101:
+
+import List (insert)
+
+insertionSort :: Ord a => [a] -> [a]
+insertionSort = foldl (\ a b -> insert b a) []
+
+-- What? That's crazy! But GHC's merge sort is epic:
+
+sort = sortBy compare
+sortBy cmp = mergeAll . sequences
+  where
+    sequences (a:b:xs)
+      | a `cmp` b == GT = descending b [a]  xs
+      | otherwise       = ascending  b (a:) xs
+    sequences xs = [xs]
+
+    descending a as (b:bs)
+      | a `cmp` b == GT = descending b (a:as) bs
+    descending a as bs  = (a:as): sequences bs
+
+    ascending a as (b:bs)
+      | a `cmp` b /= GT = ascending b (\ys -> as (a:ys)) bs
+    ascending a as bs   = as [a]: sequences bs
+
+    mergeAll [x] = x
+    mergeAll xs  = mergeAll (mergePairs xs)
+
+    mergePairs (a:b:xs) = merge a b: mergePairs xs
+    mergePairs xs       = xs
+
+    merge as@(a:as') bs@(b:bs')
+      | a `cmp` b == GT = b:merge as  bs'
+      | otherwise       = a:merge as' bs
+    merge [] bs         = bs
+    merge as []         = as
+
+-- if you thought Haskell was always super-brief, there's your counter-example.
